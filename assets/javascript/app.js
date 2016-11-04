8f677b80b1eb50ee9454a51f17b9faf3dcee852b
Patrick Segura <psegur@Patricks-MacBook-Pro-2.local>
2016-11-04 16:23:56 -0500

    Final3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+// object of all questions, choices, and answers
     var game = [{
         question: 'What has happened to images?',
         choices: ['devoured', 'shat on', 'vommited on', 'ravaged'],
@@ -18,6 +19,7 @@ $(document).ready(function() {
         answer: 'multiverse'
     }];
 
+// poem iterated through with a counter/delay, matched to the vocal audio
     var poem = [
         '',
         'Degredation followed display.',
@@ -50,32 +52,46 @@ $(document).ready(function() {
         'and channel houses.',
     ];
 
+// stores users choice
     var userGuess;
+// current question chosen from the random function
     var currentQ;
+// an array storing all the random questions guessed
     var used = [];
+// the counter/timer resets to this number every time
     var number = 30;
+// hold the amount to skip video by
     var vidSkip = 20;
-    var vidCount = 0;
+// stored user input from textarea
     var userInput = '';
+// index of poem
     var poemIndex = 0;
+// specific variable that is modified to control specific video switches
     var switchTime = 95;
+
+// audio and video elements grabbed and stored
     var video = document.getElementById('vid');
     var aud = document.getElementById('aud');
     // aud.volume = 0;
 
+// on click starts the game
     $('body').one('click', function(){
         startGame();
     });
 
+// plays audio, video, and begins the first story
     function startGame() {
         aud.play();
         video.play();
         runStory();
     }
 
+// chooses a random question from an array and stores that value into a new array 
+// so that the random choice is not chosen again
     function randomQuestion() {
         var rand = Math.floor((Math.random() * 4));
         var x = $.inArray(rand, used);
+        // if not already chosen, chooses, then push into new array
         if (x == -1) {
             used.push(rand);
             currentQ = game[rand];
@@ -83,10 +99,12 @@ $(document).ready(function() {
             reset();
             return;
         } else {
+            // if already chosen, runs the function again
             randomQuestion();
         }
     }
 
+// sets the timer, prints the question and new choices
     function setupQ() {
         $('#timer').html('30');
         $('.choices').empty();
@@ -97,6 +115,8 @@ $(document).ready(function() {
         }
     }
 
+// on click of each choices, establishes if correct/incorrect.  if correct, resets the story 
+// and prints the correct answer, if wrong, runs wrong answer
     $('.choices').on('click', function() {
         userGuess = $(this).html();
         if (userGuess === '') {
@@ -112,8 +132,11 @@ $(document).ready(function() {
         }
     });
 
+// user can also submit answer through textarea. establishes if correct/incorrect.  if correct, resets the story 
+// and prints the correct answer, if wrong, runs wrong answer
     function submit() {
         $('#inputBox').keydown(function(event) {
+            // if "enter" key is pressed inside the textarea
             if (event.keyCode == 13) {
                 event.preventDefault();
                 userInput = document.getElementById('inputBox').value;
@@ -128,36 +151,45 @@ $(document).ready(function() {
         });
     }
 
+// fades in and out the correct answer over the entire screen
     function correctAnswer() {
         $('#over').fadeIn();
         $('#over').html(currentQ.answer).css('zIndex', 7).delay(1000).fadeOut('slow');
     }
 
+// fades in and out that you answered incorrectly over the entire screen
     function wrongAnswer() {
         $('#over').fadeIn();
         $('#over').html("Your choice injured you.").css('zIndex', 7).delay(1000).fadeOut('slow');
     }
 
+// runs the timer of decrementQuestion
     function runStory() {
         counter = setInterval(decrementQuestion, 1000);
     }
 
+// runs the timer of decrementStory
     function runQuestions() {
         counter = setInterval(decrementStory, 1000);
     }
 
-    // The decremeent function.
+// The decremeent function.
     function decrementQuestion() {
+        // stores each tick of timer and compares it to the previous one to make sure it changed.
+        // used to trigger the poem with each index of array
         var oldNumber = number;
         number--;
         $('#timer').html('<h2>' + number + '</h2>');
+        // special trigger for certain time to skip video/audio
         if (number === 0 && aud.currentTime > 290 && aud.currentTime < 320) {
             $('#inputBox').val('');
             resetQuestion();
             poemIndex = 0;
+            // gains power to skip video and displays text explaining
             videoControl();
             $('#over').fadeIn();
             $('#over').html("You have gained a new power. Remix time (right or left)").css({'zIndex': 7, "fontSize": "75px"}).delay(3000).fadeOut('slow');
+        // special trigger for certain time to skip video/audio
         } else if (number === 0 && aud.currentTime > 327 && used.length !== 4) {
             $('#inputBox').val('');
             resetQuestion();
@@ -165,18 +197,22 @@ $(document).ready(function() {
             videoControl();
         } else if (number === 0 && used.length !== 4) {
             aud.pause();
+            // remove initial draw canvas
             $('#canvasDiv').remove();
             resetQuestion();
             poemIndex = 0;
+            // resets if all questions have been asked(array = 4)
         } else if (number === 0 && used.length == 4) {
             reset();
         }
+        // on each number change, print index of array to textarea
         if (number !== oldNumber) {
             $('#inputBox').val(poem[poemIndex]);
             poemIndex += 1;
         }
     }
 
+// counts down through the story, when number = 0, resets story
     function decrementStory() {
         number--;
         $('#timer').html('<h2>' + number + '</h2>');
@@ -188,11 +224,13 @@ $(document).ready(function() {
             $('#over').html("Time Up.").css('zIndex', 7).delay(1000).fadeOut('slow');
         }
     }
-    // The stop function
+
+// The stop function
     function stop() {
         clearInterval(counter);
     }
 
+// runs the story, switching audio at appropriate times.  clears the questions and choices in the DOM
     function resetStory() {
         stop();
         number = 30;
@@ -207,6 +245,7 @@ $(document).ready(function() {
         $('.choices').empty();
     }
 
+// prints the question and choices to the screen
     function resetQuestion() {
         stop();
         number = 30;
@@ -219,6 +258,7 @@ $(document).ready(function() {
         submit();
     }
 
+// certain points in story/questions where video needs to be shifted and dom elements adjusted
     function vidShifts() {
         if (video.currentTime > 31 && video.currentTime < 61) {
             video.currentTime = 65;
@@ -230,7 +270,8 @@ $(document).ready(function() {
         } 
     }
 
-    // video controller - fast forward and rewinds with right and left arrow keys
+// video controller - fast forward and rewinds with right and left arrow keys
+// this ability only gained at a certain point in story
     function videoControl() {
         $(document).keydown(function(event) {
             if (event.keyCode == 39) {
@@ -243,6 +284,7 @@ $(document).ready(function() {
         });
     }
 
+// resets to beginning of game with no refresh
     function reset() {
         stop();
         video.pause();
@@ -252,9 +294,9 @@ $(document).ready(function() {
         number = 30;
         used = [];
         vidSkip = 20;
-        vidCount = 0;
         userInput = '';
         poemIndex = 0;
+        switchTime = 95;
         $('#inputBox').css('marginBottom', '415px').val('');
         $('#timer').html('');
         $('#over').fadeIn();
